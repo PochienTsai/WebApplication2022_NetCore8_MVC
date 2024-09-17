a63afa368f47eb349d0c66d7fdce0340272aace5
PochienTsai <bernie7135@gmail.com>
2024-09-17 21:31:11 +0800

    簡化程式碼並更新資料庫連接字串
    在 `Program.cs` 中，移除了多餘的註解和未使用的程式碼，簡化了 `app.Run()` 的呼叫。保留了從 `appsettings.json` 中讀取連接字串的設定方法。
    
    在 `appsettings.json` 中，更新了 `DefaultConnection` 的連接字串，將伺服器名稱從 `.\\sqlexpress` 改為 `MSI`，並將舊的連接字串註解掉。

--- Program.cs
+++ Program.cs
@@ -2,23 +2,16 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;     // 請自己手動安裝NuGet -- ConfigurationBuilder會用到這個命名空間
 using Microsoft.Extensions.DependencyInjection;
-
 using WebApplication2022_NetCore8_MVC.Models;  // 請自己手動加入這個命名空間。
-
-
-
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 // 自己動手加入的段落  (DI)
 #region
 //**** 讀取 appsettings.json 設定檔裡面的資料（資料庫連結字串）****
 ////作法一：
 //builder.Services.AddDbContext<MvcUserDbContext>(options => options.UseSqlServer("Server=.\\sqlexpress;Database=MVC_UserDB;Trusted_Connection=True;TrustServerCertificate=true;MultipleActiveResultSets=true"));
 //// 這裡需要新增兩個命名空間，請使用「顯示可能的修正」讓系統自己加上。
-
 ////作法二： 讀取設定檔的內容
 //// 資料來源  程式碼  https://github.com/CuriousDrive/EFCore.AllDatabasesConsidered/blob/main/MS%20SQL%20Server/Northwind.MSSQL/Program.cs
 builder.Services.AddDbContext<MvcUserDbContext>(
@@ -26,20 +19,15 @@ builder.Services.AddDbContext<MvcUserDbContext>(
         {
             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
         });
-
-
 ////作法三： 讀取設定檔的內容
 ////****  資料庫連結字串 **** 讀取 appsettings.json 設定檔裡面的資料（資料庫連結字串）****
 //// (3-A) 可運作
 //var configurationBuilder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json");
 //IConfiguration config = configurationBuilder.Build();   // ConfigurationBuilder會用到 Microsoft.Extensions.Configuration命名空間
 //builder.Services.AddDbContext<MVC_UserDB2Context>(options => options.UseSqlServer(config["ConnectionStrings:DefaultConnection"]));  // appsettings.josn檔裡面的「資料庫連結字串」
-
 //// (3-B) 可運作
 //IConfiguration config = builder.Configuration;   // ConfigurationBuilder會用到 Microsoft.Extensions.Configuration命名空間
 //builder.Services.AddDbContext<MVC_UserDB2Context>(options => options.UseSqlServer(config.GetConnectionString("DefaultConnection")));  // appsettings.josn檔裡面的「資料庫連結字串」
-
-
 //======================
 //使用完成，會"自動"關閉資料庫連線。 https://docs.microsoft.com/zh-tw/aspnet/core/data/ef-mvc/crud?view=aspnetcore-5.0
 //======================
@@ -48,12 +36,8 @@ builder.Services.AddDbContext<MvcUserDbContext>(
 // Scoped 表示內容物件的存留期會與 Web 要求的存留期保持一致，並且在 Web 要求結束時會自動呼叫 Dispose 方法。
 //********************************************************************
 #endregion
-
-
 //==== 分 隔 線 =============================
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
@@ -61,16 +45,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
